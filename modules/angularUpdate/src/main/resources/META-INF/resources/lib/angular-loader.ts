9018b9a8cdc3ee9d2a22ecc2db691007d8f47395
Gustavo Mantuan <gustavo.mantuan@gs.liferay.com>
2020-09-04 18:32:41 -0300

    update package.json version, update ts requireded values, remove import core-js as it is default

--- modules/angularUpdate/src/main/resources/META-INF/resources/lib/angular-loader.ts
+++ modules/angularUpdate/src/main/resources/META-INF/resources/lib/angular-loader.ts
@@ -1,4 +1,3 @@
-import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
 declare var Liferay: any;
--- modules/angularUpdate/src/main/resources/META-INF/resources/lib/app/app.component.ts
+++ modules/angularUpdate/src/main/resources/META-INF/resources/lib/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 export class Hero {
-	id: number;
-	name: string;
+	id!: number;
+	name!: string;
 }
 
 @Component({
