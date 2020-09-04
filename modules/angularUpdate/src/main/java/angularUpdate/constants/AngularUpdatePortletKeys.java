85204b3750a188393ff54c074e7a7cef5b2e9448
Gustavo Mantuan <gustavo.mantuan@gs.liferay.com>
2020-09-04 18:10:44 -0300

    first commit with blade angular portlet template default version

---
+++ modules/angularUpdate/src/main/java/angularUpdate/constants/AngularUpdatePortletKeys.java
@@ -0,0 +1,10 @@
+package angularUpdate.constants;
+
+/**
+ * @author gustavo.mantuan
+ */
+public class AngularUpdatePortletKeys {
+
+	public static final String AngularUpdate = "angularupdate";
+
+}
